5b68f2242e3bd8025772822d74ce8839ef6ab5af
sahrulms <45790745+sahrulms@users.noreply.github.com>
2022-09-08 17:39:02 +0700

    Delete script.js

--- Galeri/script.js
+++
@@ -1,9 +0,0 @@
-const elementclose = document.getElementsByClassName('overlay');
-
-for (let i = 0;i< elementclose.length; i++ ) {
-    elementclose[i].addEventListener('click',function(){
-        let url = window.location.href.substr(0, window.location.href.indexOf('#'))
-        window.location = url;
-    });
-
-} 
